52543a146e33a155fd89f3e71cd9b892f5fe69f9
Denny <denny1011133@gmail.com>
2020-05-16 14:52:09 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+// 引用路由器
+const routes = require('./routes')
+// 將 request 導入路由器
+app.use(routes)
 // 載入 Todo model
 const Todo = require('./models/todo')
 // 設定樣板
@@ -28,60 +32,60 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
-// 設定首頁路由
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
+// // 設定首頁路由
+// app.get('/', (req, res) => {
+//   Todo.find() // 取出 Todo model 裡的所有資料
+//     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+//     .sort({ _id: 'asc' })
+//     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+//     .catch(error => console.error(error)) // 錯誤處理
+// })
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
+// app.get('/todos/new', (req, res) => {
+//   return res.render('new')
+// })
 
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-app.get('/todos/:id', (req, res) => {
-  console.log(req.params.id)
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  console.log(req.body)
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// app.post('/todos', (req, res) => {
+//   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+//   return Todo.create({ name })     // 存入資料庫
+//     .then(() => res.redirect('/')) // 新增完成後導回首頁
+//     .catch(error => console.log(error))
+// })
+// app.get('/todos/:id', (req, res) => {
+//   console.log(req.params.id)
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('detail', { todo }))
+//     .catch(error => console.log(error))
+// })
+// app.get('/todos/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .lean()
+//     .then((todo) => res.render('edit', { todo }))
+//     .catch(error => console.log(error))
+// })
+// app.put('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   const { name, isDone } = req.body
+//   console.log(req.body)
+//   return Todo.findById(id)
+//     .then(todo => {
+//       todo.name = name
+//       todo.isDone = isDone === 'on'
+//       return todo.save()
+//     })
+//     .then(() => res.redirect(`/todos/${id}`))
+//     .catch(error => console.log(error))
+// })
+// app.delete('/todos/:id', (req, res) => {
+//   const id = req.params.id
+//   return Todo.findById(id)
+//     .then(todo => todo.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
