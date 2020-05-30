88a648f8d001877f10cb524e7f9e319a2ebc8ec2
Denny <denny1011133@gmail.com>
2020-05-30 21:28:03 +0800

    Add user router, and login and register templates

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,13 @@ const router = express.Router()
 const home = require('./modules/home')
 // 引入 todos 模組程式碼
 const todos = require('./modules/todos')
+// 引入 user 模組程式碼
+const user = require('./modules/user')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+// 將網址結構符合 /user 字串開頭的 request 導向 home 模組 
+router.use('/user', user)
 // 匯出路由器
 module.exports = router
