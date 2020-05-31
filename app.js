02912b9706ed8581d4d5103f17534f2b26d8d8ce
Denny <denny1011133@gmail.com>
2020-05-31 09:18:46 +0800

    Install express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 // 引入總路由器
 const routes = require('./routes')
 require('./config/mongoose')
@@ -11,8 +12,14 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'your secret key',   // secret: 定義一組屬於你的字串做為私鑰
+  resave: false,
+  saveUninitialized: true,
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 // 將 request 導入路由器
 app.use(routes)
 app.use('/users', require('./routes/user'))
